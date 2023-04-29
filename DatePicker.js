50f32a0c7e41414a3d223636c13c387c67004b8e
brendanmclaughlin <bmclaughlin0407@gmail.com>
2023-04-28 20:26:31 -0700

    shows date selected

--- DatePicker.js
+++ DatePicker.js
@@ -7,15 +7,47 @@ class DatePicker {
     this.currentDate = new Date();
     this.selectedDate = null;
 
-    //const datePickerElement = document.getElementById(this.id);
-    // datePickerElement.addEventListener(
-    //   "click",
-    //   this.handleDatePickerClick.bind(this)
-    // );
+    const datePickerElement = document.getElementById(this.id);
+    datePickerElement.className = "container";
+    datePickerElement.addEventListener(
+      "click",
+      this.handleDatePickerClick.bind(this)
+    );
   }
-
   render(date) {
     this.currentDate = new Date(date);
+    this.renderDatePicker();
+  }
+
+  handleDatePickerClick(event) {
+    event.stopPropagation();
+
+    const target = event.target;
+    if (target.classList.contains("previous")) {
+      this.currentDate.setMonth(this.currentDate.getMonth() - 1);
+      console.log(this.currentDate);
+      this.renderDatePicker();
+    } else if (target.classList.contains("next")) {
+      this.currentDate.setMonth(this.currentDate.getMonth() + 1);
+      this.renderDatePicker();
+    } else if (target.classList.contains("dayCell")) {
+      const day = +target.textContent;
+      if (
+        typeof day === "number" &&
+        !target.classList.contains("not-current-month")
+      ) {
+        this.selectedDate = {
+          month: this.currentDate.getMonth() + 1,
+          day: day,
+          year: this.currentDate.getFullYear(),
+        };
+        this.callback(this.id, this.selectedDate);
+        this.renderDatePicker();
+      }
+    }
+  }
+
+  renderDatePicker() {
     const monthNames = [
       "January",
       "February",
@@ -33,16 +65,31 @@ class DatePicker {
     const datePickerElement = document.getElementById(this.id);
     datePickerElement.innerHTML = "";
 
-    const monthYearHeader = document.createElement("div");
+    const datePickerHeader = document.createElement("div");
+    datePickerHeader.className = "header";
+    datePickerElement.appendChild(datePickerHeader);
+
+    const monthYearHeader = document.createElement("span");
     monthYearHeader.className = "month-year-header";
     monthYearHeader.textContent =
-      monthNames[date.getMonth()] + " " + date.getFullYear();
-    datePickerElement.appendChild(monthYearHeader);
+      monthNames[this.currentDate.getMonth()] + " " + this.currentDate.getFullYear();
+    datePickerHeader.appendChild(monthYearHeader);
+
+    const prevButton = document.createElement("button");
+    prevButton.className = "previous";
+    prevButton.textContent = "<";
+    datePickerHeader.appendChild(prevButton);
+
+    const nextButton = document.createElement("button");
+    nextButton.className = "next";
+    nextButton.textContent = ">";
+    datePickerHeader.appendChild(nextButton);
 
     const table = document.createElement("table");
     datePickerElement.appendChild(table);
 
     const headerRow = document.createElement("tr");
+    headerRow.clasName = "header-row-days";
     table.appendChild(headerRow);
 
     // make header row
@@ -50,11 +97,15 @@ class DatePicker {
     for (const day of daysOfWeek) {
       const headerCell = document.createElement("th");
       headerCell.innerHTML = day;
+      headerCell.className = "header-day";
       headerRow.appendChild(headerCell);
     }
 
-    const month = date.getMonth();
-    const year = date.getFullYear();
+
+    // new method
+
+    const month = this.currentDate.getMonth();
+    const year = this.currentDate.getFullYear();
     const firstDay = new Date(year, month, 1);
     const lastDay = new Date(year, month + 1, 0);
     const numDaysInMonth = lastDay.getDate();
@@ -73,6 +124,7 @@ class DatePicker {
       for (let j = 0; j < 7; j++) {
         const dayCell = document.createElement("td");
         dayCell.className = "dayCell";
+
         if (i === 0 && j < firstWeekday) {
           // add previous month
           dayCell.innerHTML = numDaysInPrevMonth - firstWeekday + j + 1;
@@ -84,14 +136,18 @@ class DatePicker {
             break;
           }
           dayCell.innerHTML = nextMonthDay;
+          dayCell.classList.add("not-current-month");
           nextMonthDay++;
         } else {
           dayCell.innerHTML = currentDay;
+          // dayCell.addEventListener("click", alert("hi"));
           if (currentDay === this.currentDate.getDate()) {
             dayCell.classList.add("current-day");
           }
-          
+
           currentDay++;
+          // this.callback(this.id, {month: this.currentDate.getMonth(),
+          // day: currentDay, year: this.currentDate.getYear()})
         }
         weekRow.appendChild(dayCell);
       }
@@ -109,4 +165,5 @@ class DatePicker {
       //   }
     }
   }
+  
 }
